722042ac4fd1b7555ef3eb7d3e4aa805bc998aa6
David <davidrecheni@gmail.com>
2024-05-25 19:59:02 +0200

    Finalized contracts

--- contracts/hoarder.sol
+++ contracts/hoarder.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.25;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 
-contract Hoarder is Ownable, Pausable, ReentrancyGuard, IERC721Receiver {
+contract Hoarder is Ownable, Pausable, ReentrancyGuard {
+    constructor() Ownable(msg.sender) {}
+
     using Address for address;
     using ERC165Checker for address;
 
@@ -26,6 +28,11 @@ contract Hoarder is Ownable, Pausable, ReentrancyGuard, IERC721Receiver {
         uint256 tokenId;
     }
 
+    struct Currency {
+        address tokenAddress;
+        uint256 amount;
+    }
+
     /**
      * Event emitted when ETH is sent to the contract.
      */
@@ -37,21 +44,23 @@ contract Hoarder is Ownable, Pausable, ReentrancyGuard, IERC721Receiver {
     event NFTsHarvested(address indexed sender, NFT[] tokens);
 
     /**
+     * Event emitted wieh fungible token(s) are harvested by the contract */
+    event CurrenciesHarvested(address indexed sender, Currency[] tokens);
+
+    /**
      * Function which enables external consumers to sell
      * multiple ERC-721 NFTs to the contract. By calling this function,
      * the contract will transfer the tokens to itself (the contract)
-     * and pay the sender a fixed amount of ETH for each token.
+     * and pay the sender a fixed amount of ETH.
      */
-    function sellNFT(NFT[] calldata tokens)
-        external
-        nonReentrant
-        whenNotPaused
-    {
+    function sellNFT(
+        NFT[] calldata tokens
+    ) external nonReentrant whenNotPaused {
         // Require 'tokens' to not be empty
         require(tokens.length > 0, "Hoarder: 'tokens' must not be empty");
 
         address payable sender = payable(msg.sender);
-        uint256 calculatedValue = tokens.length * FIXED_NFT_VALUE;
+        uint256 calculatedValue = FIXED_NFT_VALUE;
 
         // Require contract to contain enough ETH balance
         require(
@@ -89,23 +98,43 @@ contract Hoarder is Ownable, Pausable, ReentrancyGuard, IERC721Receiver {
     }
 
     /**
-     * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md#specification
-     *
-     * Always returns `IERC721Receiver.onERC721Received.selector`.
+     * Function which enables external consumers to sell
+     * multiple ERC-20 Tokens to the contract. By calling this function,
+     * the contract will transfer the tokens to itself (the contract)
+     * and pay the sender a fixed amount of ETH.
      */
-    function onERC721Received(
-        address,
-        address from,
-        uint256 tokenId,
-        bytes memory
-    ) public virtual override whenNotPaused returns (bytes4) {
-        // Require NFT to have previously been sold to the contract (msg.sender is the NFT contract address)
+    function sellCurrency(
+        Currency[] calldata tokens
+    ) external payable nonReentrant whenNotPaused {
+        // Require 'tokens' to not be empty
+        require(tokens.length > 0, "Hoarder: 'tokens' must not be empty");
+
+        address payable sender = payable(msg.sender);
+        uint256 calculatedValue = FIXED_NFT_VALUE;
+
+        // Require contract to contain enough ETH balance
         require(
-            _priorOwnerByToken[msg.sender][tokenId] == from,
-            "Hoarder: NFT must have been previously sold to the contract"
+            address(this).balance >= calculatedValue,
+            "Hoarder: Insufficient Balance"
         );
 
-        return this.onERC721Received.selector;
+        for (uint256 i = 0; i < tokens.length; ++i) {
+            address tokenAddress = tokens[i].tokenAddress;
+            uint256 amount = tokens[i].amount;
+
+            IERC20 currency = IERC20(tokenAddress);
+
+            uint256 allowance = currency.allowance(msg.sender, address(this));
+            require(allowance >= amount, "Insufficient allowance");
+
+            currency.transferFrom(sender, address(this), amount);
+        }
+
+        // Emit event
+        emit CurrenciesHarvested(sender, tokens);
+
+        // Transfer ETH to sender
+        Address.sendValue(sender, calculatedValue);
     }
 
     /**
@@ -133,11 +162,10 @@ contract Hoarder is Ownable, Pausable, ReentrancyGuard, IERC721Receiver {
     /**
      * Withdrawal ETH funds to the given recipient address.
      */
-    function withdraw(address payable recipient, uint256 amount)
-        external
-        nonReentrant
-        onlyOwner
-    {
+    function withdraw(
+        address payable recipient,
+        uint256 amount
+    ) external nonReentrant onlyOwner {
         require(amount > 0, "Hoarder: 'amount' must be greater than zero");
         require(
             recipient != address(this),
